57ce51e0ee15215e017cdc03f8ce7fdcbdabb8df
Mauricio Noreña <baphometh@gmail.com>
2017-09-04 10:35:31 -0500

    added solution to form filtering from expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,20 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    if params[:concept] == ''
+      params[:concept] = nil
+    end
+    if params[:category_id] == ''
+      params[:category_id] = nil
+    end
+    if params[:concept] && params[:category_id]
+      @expenses = Expense.where("concept like ?", "%" + params[:concept] + "%")
+      @expenses = @expenses.where( category_id: params[:category_id] )
+    elsif params[:concept]
+      @expenses = Expense.where("concept like ?", "%" + params[:concept] + "%")
+    elsif params[:category_id]
+      @expenses = Expense.where( category_id: params[:category_id] )
+    else
+      @expenses = Expense.order("date DESC")
+    end
   end
 end
